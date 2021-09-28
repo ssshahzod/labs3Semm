ce729b3bb2cbac058e37424a9a2536e500114b93
ssshahzod <55853125+ssshahzod@users.noreply.github.com>
2021-09-28 14:18:32 +0300

    Delete 2nd/cmake-build-debug directory

--- 2nd/cmake-build-debug/lib/googletest-master/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/cronion/Documents/labs/lab3sem/2nd/lib/googletest-master/googlemock
-# Build directory: /home/cronion/Documents/labs/lab3sem/2nd/cmake-build-debug/lib/googletest-master/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
--- 2nd/cmake-build-debug/lib/googletest-master/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/cronion/Documents/labs/lab3sem/2nd/lib/googletest-master/googletest
-# Build directory: /home/cronion/Documents/labs/lab3sem/2nd/cmake-build-debug/lib/googletest-master/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
